8821e0d16af5de7a82f121bc646e43477c0a6a6b
MosesPe <ipekar@gmail.com>
2022-10-26 07:26:33 -0400

    We did not need axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import axios from 'axios'
+
 function App() {
 
 
   return (
     <div className="App">
-     
+     <input type="url" name="url" id="url"></input>
+
     </div>
   )
 }
