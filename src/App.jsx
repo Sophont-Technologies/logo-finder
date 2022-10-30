a30a5980624f9c1af7b6d37e44104cd20dca5f93
MosesPe <ipekar@gmail.com>
2022-10-30 19:07:12 -0400

    added an input and a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import axios from 'axios'
-
+import {usestate} from 'react'
 function App() {
 
 
   return (
     <div className="App">
      <input type="url" name="url" id="url"></input>
-
+<button>enter</button>
     </div>
   )
 }
