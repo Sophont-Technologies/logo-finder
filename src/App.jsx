6aa46d7727a18732ef39c65b89e3f279dd574083
Yosef Herskovitz <yosef.herskovitz@hearst.com>
2022-10-30 19:59:49 -0400

    removed axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import './App.css'
-import axios from 'axios'
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react";
 function App() {
-const [name, setName] = useState('')
-const [url,setUrl] = useState('')
-const handleChange = (event) => {
- setName(event.target.value); 
-
-}
-const getlogo = () => {
-//usestate seturl to template literal
-setUrl(`https://logo.clearbit.com/${name}`)
-}
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+  const getlogo = () => {
+    //usestate seturl to template literal
+    setUrl(`https://logo.clearbit.com/${name}`);
+  };
   return (
     <div className="App">
-     <input type="url" name="url" id="url" onChange={handleChange}></input>
+      <input type="url" name="url" id="url" onChange={handleChange}></input>
 
-<button onClick={getlogo}>enter</button>
-<img src={url} alt="" />
+      <button onClick={getlogo}>enter</button>
+      <img src={url} alt="" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
