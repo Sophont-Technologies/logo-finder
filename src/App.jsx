dca6d7f6136d668e2561360a16d533e94cc1aae3
MosesPe <ipekar@gmail.com>
2022-10-30 19:42:39 -0400

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 import axios from 'axios'
-import {usestate} from 'react'
+import {useState} from 'react'
 function App() {
+const [name, setName] = useState('')
+const [url,setUrl] = useState('')
+const handleChange = (event) => {
+ setName(event.target.value); 
 
-
+}
+const getlogo = () => {
+//usestate seturl to template literal
+setUrl(`https://logo.clearbit.com/${name}`)
+}
   return (
     <div className="App">
-     <input type="url" name="url" id="url"></input>
+     <input type="url" name="url" id="url" onChange={handleChange}></input>
 
-<button>enter</button>
-
+<button onClick={getlogo}>enter</button>
+<img src={url} alt="" />
     </div>
   )
 }
