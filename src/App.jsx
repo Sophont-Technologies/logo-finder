a1ce892c3f491b933f22efb1b54e1e48a41465c5
MosesPe <ipekar@gmail.com>
2022-11-02 17:03:15 -0400

    I styled the page up a little

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,18 @@ setUrl(`https://logo.clearbit.com/${name}`)
 }
   return (
     <div className="App">
-     <input type="url" name="url" id="url" onChange={handleChange}></input>
+      <header>
+      <h1>Logo Finder</h1>
+<h4>Enter company's website and get their logo!</h4>
+      </header>
+
+<div id='btn'>
+<input type="url" name="url" id="url" onChange={handleChange} placeholder='enter company website'></input>
 
 <button onClick={getlogo}>enter</button>
-<img src={url} alt="" />
+</div>
+    
+<img draggable='false' src={url} alt="logo not found" id='a' />
     </div>
   )
 }
